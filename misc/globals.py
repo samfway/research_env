6bc9eb545a10f799f225573e41b625100c4266e7
Sam Way <samfway@gmail.com>
2017-04-09 16:32:29 -0600

    Automatically load the primary data

--- misc/globals.py
+++ misc/globals.py
@@ -26,11 +26,11 @@ from scipy import stats
 # Seed the random number generator (#reproducibility)
 np.random.seed(9)
 
-
 # Project directory setup
 PROJ_DIR = '/Users/samfway/Documents/Work/ClausetLab/Projects/research_env/'
 FIGS_DIR = os.path.join(PROJ_DIR, 'figures')
 DATA_DIR = os.path.join('/Users/samfway/Documents/Work/ClausetLab/Projects/faculty_hiring/data/')
+DBLP_DIR = '/Users/samfway/Documents/Work/ClausetLab/projects/faculty_hiring/data/profiles_DBLP_Nov16/'
 
 BS_FACULTY_FILE = os.path.join(DATA_DIR, 'faculty_bs_CURRENT.txt') 
 CS_FACULTY_FILE = os.path.join(DATA_DIR, 'faculty_cs_CURRENT.txt') 
@@ -40,5 +40,43 @@ BS_INST_FILE = os.path.join(DATA_DIR, 'inst_bs_CURRENT.txt')
 CS_INST_FILE = os.path.join(DATA_DIR, 'inst_cs_CURRENT.txt')
 HS_INST_FILE = os.path.join(DATA_DIR, 'inst_hs_CURRENT.txt')
 
+
 # Colors
 ACCENT_COLOR_1 = np.array([176., 116., 232.]) / 255.
+
+# Load the standard set of files
+# Business
+bs_inst = institution_parser.parse_institution_records(open(BS_INST_FILE, 'rU'))
+all_bs_faculty = [person for person in faculty_parser.parse_faculty_records(open(BS_FACULTY_FILE, 'rU'),
+                                                                            school_info=bs_inst,
+                                                                            ranking='pi')]
+bs_faculty = load.load_assistant_profs(open(BS_FACULTY_FILE, 'rU'), 
+                                       school_info=bs_inst, 
+                                       ranking='pi', 
+                                       year_start=1970, 
+                                       year_stop=2012)
+# bs_faculty_df = convert_faculty_list_to_df(bs_faculty)
+
+# Computer Science
+cs_inst = institution_parser.parse_institution_records(open(CS_INST_FILE, 'rU'))
+all_cs_faculty = [person for person in faculty_parser.parse_faculty_records(open(CS_FACULTY_FILE, 'rU'),
+                                                                            school_info=cs_inst,
+                                                                            ranking='pi')]
+cs_faculty = load.load_assistant_profs(open(CS_FACULTY_FILE, 'rU'), 
+                                       school_info=cs_inst, 
+                                       ranking='pi', 
+                                       year_start=1970, 
+                                       year_stop=2012)
+# cs_faculty_df = convert_faculty_list_to_df(cs_faculty)
+
+# History
+hs_inst = institution_parser.parse_institution_records(open(HS_INST_FILE, 'rU'))
+all_hs_faculty = [person for person in faculty_parser.parse_faculty_records(open(HS_FACULTY_FILE, 'rU'),
+                                                                            school_info=hs_inst,
+                                                                            ranking='pi')]
+hs_faculty = load.load_assistant_profs(open(HS_FACULTY_FILE, 'rU'), 
+                                       school_info=hs_inst, 
+                                       ranking='pi', 
+                                       year_start=1970, 
+                                       year_stop=2012)
+# hs_faculty_df = convert_faculty_list_to_df(hs_faculty)
